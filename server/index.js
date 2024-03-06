407f272409d76337063d86dcac97f4c55f7b40ca
vinhphuphan <vinhphu.phan@students.mq.edu.au>
2024-03-06 21:37:03 +0700

    Create server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config()
 const app = express();
 const port = 8080;
+const connect = () => {
+    mongoose
+        .connect(process.env.MONGO)
+        .then(() => {
+            console.log("Connected to DB!")
+        })
+        .catch((error) => {
+            throw error;
+        })
+}
 
 app.listen(port, () => {
+    connect();
     console.log("Connected!")
 })
